9445e1443e550911eb8b54fed77f2d85d8945b56
risavkarna <risavkarna@gmail.com>
2020-05-14 15:48:07 +0545

    add basic auth with local storage

--- dashboard/src/app/services/auth/auth.service.ts
+++ dashboard/src/app/services/auth/auth.service.ts
@@ -1,37 +1,92 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { DBAuthResponse } from '../../models/response.model';
+import { CurrentUser } from '../../models/domain.model';
+import { BasicAuth, DBAuthResponse } from '../../models/response.model';
 import { EnvironmentService } from '../env/environment.service';
-import { AuthCookieService } from './auth-cookie.service';
+import { map, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class DBAuthService {
+export class AuthService {
 
-  constructor(private cookieService: AuthCookieService,
+  private isAuthenticated_ = false;
+  private user_: string;
+  private pass_: string;
+
+  constructor(
     private http: HttpClient,
-    private environment: EnvironmentService) { }
+    private environment: EnvironmentService,
+    ) { }
 
-  dashboardAuthRequest() {
-    return this.userAuthRequest(this.environment.dashboardUser, this.environment.dashboardPassword);
+  login(username: string, password: string): Observable<BasicAuth.Response> {
+    return this.basicAuthRequest(username, password, true);
   }
 
-  // tslint:disable: no-console
-  userAuthRequest(username: string, password: string) {
+  basicAuthRequest(username: string, password: string, refreshCredentials: boolean) {
+    const base64AuthString = btoa(`${username}:${password}`);
+    return this.http.get<BasicAuth.Response>(this.environment.authUri, {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Basic ${base64AuthString}`,
+        },
+      }).pipe(tap(response => {
+        if (refreshCredentials) {
+          this.refreshCurrentUserCredentials(BasicAuth.isSuccess(response), username, password);
+        }
+      }));
+  }
+
+  private refreshCurrentUserCredentials(isAuthenticated: boolean, user: string, pass: string) {
+    if (isAuthenticated) {
+      this.setCredentials(user, pass);
+    } else {
+      this.removeCredentials();
+    }
+  }
+
+  private setCredentials(username: string, password: string): void {
+    this.user_ = username;
+    this.pass_ = password;
+    this.isAuthenticated_ = true;
+    Object.entries({ username, password, isLoggedIn: 'true' })
+      .forEach(([key, val]) => localStorage.setItem(key, val));
+  }
+
+  logout(): void {
+    this.removeCredentials();
+  }
+
+  private removeCredentials(): void {
+    this.user_ = null;
+    this.pass_ = null;
+    this.isAuthenticated_ = false;
+    Object.values(CurrentUser).forEach(localStorage.removeItem);
+  }
+
+  get user(): string {
+    if (this.user_) return this.user_;
+    return localStorage.getItem(CurrentUser.name);
+  }
+
+  get pass(): string {
+    if (this.user_) return this.pass_;
+    return localStorage.getItem(CurrentUser.pass);
+  }
+
+  get isAuthenticated(): boolean {
+    if (this.isAuthenticated_) return this.isAuthenticated_;
+    return localStorage.getItem(CurrentUser.isLoggedIn) ? true : false ;
+  }
+
+  // TODO remove when jwt setup is fixed
+  userAuthRequestCookieBased(username: string, password: string) {
     this.http.post<DBAuthResponse>(this.environment.authUri,
       { username,
         password,
       }).subscribe(response => {
-        this.cookieService.create(response);
+        // this.cookieService.create(response);
       });
-      // const responseObs = this.http.post(this.environment.authUri,
-      //     'client-id=team-service&username=test&password=test&grant_type=password&scope=openid',
-      //   {
-      //     headers: new HttpHeaders({
-      //       'Content-Type': 'application/x-www-form-urlencoded',
-      //     }),
-      //     observe: 'response',
-      //   });
   }
 }
