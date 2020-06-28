9f9cccdf2775ed88a10618a5aa3c309397512ff9
risavkarna <risavkarna@gmail.com>
2020-06-28 22:50:06 +0545

    add census and health stats types

--- dashboard/src/app/models/db-response.model.ts
+++ dashboard/src/app/models/db-response.model.ts
@@ -74,3 +74,89 @@ export interface DBErrorResponse {
     error: string;
     reason: string;
 }
+
+
+export module Census2011 {
+
+  export interface District {
+      id: number;
+      district: string;
+      households: number;
+      total: number;
+      male: number;
+      female: number;
+  }
+
+  export interface Districts {
+      _id: 'district_stats_2011';
+      _rev?: string;
+      values: District[];
+  }
+
+  export function isDistrictCensus(properties: Census2011.Districts | any):
+  properties is Census2011.Districts {
+    return (properties as Census2011.Districts)._id === 'district_stats_2011';
+  }
+
+}
+
+export module CensusUpdated {
+
+  export interface Municipality {
+      id: string;
+      description: string;
+      municipality: string;
+      population: number;
+  }
+
+  export interface Municipalities {
+      id: 'municipality_stats';
+      _rev?: string;
+      values: Municipality[];
+  }
+}
+
+export module HealthStats {
+
+  export interface District {
+      province: string;
+      district: string;
+      quarantine_total: number;
+      bed_capacity: number;
+      quarantined_total: number;
+      home_quarantine: number;
+      isolation_beds: number;
+      isolated_total: number;
+      swabs_collected_till_yesterday: number;
+      todays_swab_collection: number;
+      total_swab_collection: number;
+      awaiting_results: number;
+      male_positives: number;
+      female_positives: number;
+      total_positives: number;
+      negatives: number;
+      collected_till_yesterday: number;
+      todays_rdt_tests: number;
+      total_rdt_tests: number;
+      positives: number;
+      negatives_1: number;
+      discharged_till_yesterday: number;
+      discharged_today: number;
+      total_discharged: number;
+      unlabeled: number;
+  }
+
+  export interface Districts {
+      _id: 'district_health_stats';
+      _rev?: string;
+      values: District[];
+  }
+
+
+  export function isDistrictHealthStats(properties: HealthStats.Districts | any):
+  properties is HealthStats.Districts {
+    return (properties as HealthStats.Districts)._id === 'district_health_stats';
+  }
+
+}
+
