b2a16c9bd515171bf3220de69f6b031f6805bfab
Kripa <kripa@cellapp.co>
2020-07-14 13:55:44 +0545

    application now displays login page before dashboard

--- dashboard/src/app/auth/login.component.ts
+++ dashboard/src/app/auth/login.component.ts
@@ -32,7 +32,7 @@ export class LoginComponent implements OnInit {
       username: ['', Validators.required],
       password: ['', Validators.required],
     });
-    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/hub/home';
   }
 
   get f() {
