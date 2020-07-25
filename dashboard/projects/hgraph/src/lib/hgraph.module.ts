f80f6a39051d0bab430ed208ff4d6378b517b3e0
risavkarna <risavkarna@gmail.com>
2020-07-25 18:40:37 +0545

    add hgraph, key & meta libary port scaffolding

---
+++ dashboard/projects/hgraph/src/lib/hgraph.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { HgraphComponent } from './hgraph.component';
+
+
+
+@NgModule({
+  declarations: [HgraphComponent],
+  imports: [
+  ],
+  exports: [HgraphComponent]
+})
+export class HgraphModule { }
---
+++ dashboard/projects/meta/src/lib/meta.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { MetaComponent } from './meta.component';
+
+
+
+@NgModule({
+  declarations: [MetaComponent],
+  imports: [
+  ],
+  exports: [MetaComponent]
+})
+export class MetaModule { }
