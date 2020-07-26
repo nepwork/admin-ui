3a4667146c2bd9b54f1a91d5508595227aa2850d
Saujan Ghimire <supertrampsookah@gmail.com>
2020-07-27 00:08:26 +0100

    feat(pwa): add base files
    The manifest.webmanifest file The ngsw-config.json service worker
    Varying sizes of icons inside the assets/icons directory

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -15,12 +15,13 @@ import { NbChatModule,
   NbSidebarModule,
   NbToastrModule,
   NbWindowModule } from '@nebular/theme';
-import { AppConf, appConf } from '../environments/environment';
+import { AppConf, appConf, environment } from '../environments/environment';
 import { CoreModule } from './@core/core.module';
 import { ThemeModule } from './@theme/theme.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './auth/login.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
@@ -42,6 +43,7 @@ import { LoginModule } from './auth/login.module';
     ThemeModule.forRoot(),
     ReactiveFormsModule,
     LoginModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
   bootstrap: [AppComponent],
   providers: [{ provide: AppConf, useValue: appConf }],
