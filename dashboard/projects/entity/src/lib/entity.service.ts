ef2943522369132690ceae12cfddc4339c8ee3d2
risavkarna <risavkarna@gmail.com>
2020-08-01 08:28:18 +0545

    add observable EntityService, static GraphService

--- dashboard/projects/entity/src/lib/entity.service.ts
+++ dashboard/projects/entity/src/lib/entity.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Node } from 'projects/model/node.model';
+import { Observable } from 'rxjs';
 
+export type Idx = string | Node;
 @Injectable({
   providedIn: 'root'
 })
-export class EntityService {
+export class EntityService<T extends Idx> {
+
+  private idx: Observable<Idx>;
+  private values: Observable<Array<T>>;
 
   constructor() { }
+
+  get id(): Observable<Idx> { return this.idx; }
+
+  set id(id: Observable<Idx>) { this.idx = id; }
+
+  get val(): Observable<T[]> { return this.values; }
+
+  set val(headers: Observable<T[]>) { this.values = headers; }
+
 }
--- dashboard/projects/hgraph/src/lib/hgraph.service.ts
+++ dashboard/projects/hgraph/src/lib/hgraph.service.ts
@@ -1,37 +1,28 @@
 import { Injectable } from '@angular/core';
 
-import { Edge, BinaryEdge } from './model/edge.model';
-import { Node } from './model/node.model';
-import { Graph } from './model/hgraph.model';
+import { Edge, BinaryEdge } from 'projects/model/edge.model';
+import { Node } from 'projects/model/node.model';
+import { Graph } from 'projects/model/hgraph.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HgraphService {
-
   private nodeArr: Node[];
   private edgeArr: BinaryEdge[];
   private hedgeArr: Edge[];
 
-  constructor(private idStr: string) { }
+  constructor(private idStr: string) {}
 
-  get edges(): BinaryEdge[] { return this.edgeArr.length ? this.edgeArr.slice() : []; }
-  get hedges(): Edge[] { return this.hedgeArr.length ? this.hedgeArr.slice() : []; }
-  get id(): string { return this.idStr; }
-  get nodes(): Node[] { return this.nodeArr.length ? this.nodeArr.slice() : []; }
-  get graph(): Graph { return new Graph(this.idStr, this.nodeArr, this.edgeArr); }
-
-    /**
+  /**
    * Gets node if it exists in graph, else returns
    * new node instance from given serialized node instance
    * or new node instance with the given serialized string id
    * @param node: Node
    * @returns node if exists else returns an instance with the id from input
    */
-  getNodeInGraph(node: string): Node | undefined {
-    return (
-      this.nodes.find(n => n.id === node)
-    );
+  getNodeInGraph(node: string | Partial<Node>): Node {
+    return this.nodes.find((n) => n.id === node);
   }
 
   /**
@@ -39,52 +30,70 @@ export class HgraphService {
    * @param e : Edge with source NodeRef and target NodeRef
    * @returns edge connecting graph's existing nodes
    */
-  getEdgeInGraph(e: BinaryEdge): BinaryEdge {
-    return ({
+  getEdgeInGraph(e: Partial<BinaryEdge>): BinaryEdge {
+    return {
       id: e.id,
       from: e.from,
-      to: e.to
-    });
+      to: e.to,
+    };
   }
 
   updateNodes(nodes: Node[], remove: boolean): Node[] {
-    const inGraphNodes = nodes.map(n => this.getNodeInGraph(n.id));
+    const inGraphNodes = nodes.map((n) => this.getNodeInGraph(n.id));
     if (!remove) {
       this.nodes.push(...inGraphNodes);
     } else {
-      const inGraphNodeIds = inGraphNodes.map(n => n.id);
-      this.nodes = this.nodes.filter(
-        n => !inGraphNodeIds.includes(n.id)
-      );
+      const inGraphNodeIds = inGraphNodes.map((n) => n.id);
+      this.nodes = this.nodes.filter((n) => !inGraphNodeIds.includes(n.id));
     }
     return this.nodes;
   }
 
   updateEdges(edges: BinaryEdge[], remove: boolean): BinaryEdge[] {
-    const inGraphEdges = edges.map(e => this.getEdgeInGraph(e));
+    const inGraphEdges = edges.map((e) => this.getEdgeInGraph(e));
     if (!remove) {
       this.edges.push(...inGraphEdges);
     } else {
       this.edges = this.edges.filter(
-        se =>
+        (se) =>
           !inGraphEdges
-            .map(ie => ({ from: ie.from, to: ie.to }))
+            .map((ie) => ({ from: ie.from, to: ie.to }))
             .includes({ from: se.from, to: se.to })
       );
     }
     return this.edges;
   }
 
+  get id(): string {
+    return this.idStr;
+  }
+
+  get nodes(): Node[] {
+    return this.nodeArr.length ? this.nodeArr.slice() : [];
+  }
+
   set nodes(nodes: Node[]) {
     this.nodes = nodes || this.nodes;
+  }
+
+  set hedges(edges: Edge[]) {
+    this.hedges = edges || this.hedges;
+  }
+
+  get hedges(): Edge[] {
+    return this.hedgeArr.length ? this.hedgeArr.slice() : [];
   }
 
   set edges(edges: BinaryEdge[]) {
     this.edges = edges || this.edges;
   }
 
-  set hedges(edges: Edge[]) {
-    this.hedges = edges || this.hedges;
+  get edges(): BinaryEdge[] {
+    return this.edgeArr.length ? this.edgeArr.slice() : [];
+  }
+
+  get graph(): Graph {
+    return new Graph(this.idStr, this.nodeArr, this.edgeArr);
   }
 
 
