76bb142701a2d915709486c8121407722e252681
risavkarna <risavkarna@gmail.com>
2020-08-06 17:32:29 +0545

    (init) append

---
+++ dashboard/projects/hgraph/src/lib/stellar/append/append/append.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-append',
+  templateUrl: './append.component.html',
+  styleUrls: ['./append.component.scss']
+})
+export class AppendComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
